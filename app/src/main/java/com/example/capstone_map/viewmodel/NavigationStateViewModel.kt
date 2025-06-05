3d1085caa46e8a64bfa44741bc186254ecaad09b
leesu9042 <tnckddl8957@gmail.com>
2025-06-06 07:21:44 +0900

    feat : StateViewModel 에 공유해야할 현재위치 변수 추가
    - val currentLocation = MutableLiveData<Location>()

--- app/src/main/java/com/example/capstone_map/viewmodel/NavigationStateViewModel.kt
+++ app/src/main/java/com/example/capstone_map/viewmodel/NavigationStateViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.capstone_map.viewmodel
 
+import android.location.Location
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.capstone_map.ui.state.NavigationState
 
 class NavigationStateViewModel : ViewModel() {
+    val currentLocation = MutableLiveData<Location>()  // ✅ 올바르게 수정됨
     val navState = MutableLiveData<NavigationState>()
     val destinationText = MutableLiveData<String>()
 }
