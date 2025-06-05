db8ec8828aea62e2fa405fdf1c27204810476b0f
leesu9042 <tnckddl8957@gmail.com>
2025-06-05 19:59:31 +0900

    refactor : assembler를 활용한 액티비티 간소화

--- app/src/main/java/com/example/capstone_map/ui/NavigationActivity.kt
+++ app/src/main/java/com/example/capstone_map/ui/NavigationActivity.kt
@@ -1,35 +1,24 @@
 package com.example.capstone_map.ui
-
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
-import android.view.View // ← ✅ 추가
 import android.widget.LinearLayout
-import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.capstone_map.R
-import com.example.capstone_map.input.NavigationInputBinder
-import com.example.capstone_map.ui.helper.NavigationButtonBinder
-import com.example.capstone_map.ui.helper.TTSPlaybackHandler
-import com.example.capstone_map.ui.state.NavigationState
+import com.example.capstone_map.ui.input.NavigationInputBinder
+import com.example.capstone_map.ui.state.AwaitingDestinationInput
 import com.example.capstone_map.util.permission.PermissionHelper
-import com.example.capstone_map.viewmodel.InputViewModel
-import com.example.capstone_map.voice.STTListenerHelper
+import com.example.capstone_map.viewmodel.NavigationAssembler
 import com.example.capstone_map.voice.STTManager
 import com.example.capstone_map.voice.TTSManager
-import com.example.capstone_map.voice.TTSObserverHelper
-import com.example.capstone_map.voice.VoiceManager
-import java.util.LinkedList
-import java.util.Queue
 
 class NavigationActivity : AppCompatActivity() {
 
     private lateinit var ttsManager: TTSManager
     private lateinit var sttManager: STTManager
 
-    private val viewModel: InputViewModel by viewModels()
-    private val ttsQueue: Queue<String> = LinkedList()
+//    private val viewModel: InputViewModel by viewModels()
+    // private val ttsQueue: Queue<String> = LinkedList()
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,38 +27,24 @@ class NavigationActivity : AppCompatActivity() {
 
         PermissionHelper.requestMicrophonePermission(this) // 오디오 퍼미션 추가
 
-        // state변경 감지 바뀌면 아래 코드 실행
-        viewModel.navState.observe(this) {
-            Log.d("STATE_TRACKING", " 상태 변경됨: $it")
-        }
+        val assembler = NavigationAssembler(this, this) //어셈블러 선언
+        val destinationViewModel = assembler.destinationViewModel
+
+        ttsManager = assembler.ttsManager
+        sttManager = assembler.sttManager
+
+        destinationViewModel.updateState(AwaitingDestinationInput)
 
 
-        /**     TTS, STT 매니저 초기화
-        *       STT 결과 수신 콜백 설정
-        *       TTS 메시지 변경 감지
-         *       좀더 이해가 필요
-         */
-        ttsManager = VoiceManager.initTTS(this)
-        sttManager = VoiceManager.initSTT(this)
 
-        // STT 결과 수신 리스너 설정
-        STTListenerHelper.setListener(this, viewModel, sttManager)
+        NavigationInputBinder.bindClickAndLongPress(
+            activity = this,
+            desViewModel = destinationViewModel,
+            sttManager = sttManager,
+            buttonId = R.id.stt_button
+        )
 
 
-        // TTS 메시지 옵저버 설정
-        ttsManager.setOnTTSReadyListener {
-            TTSObserverHelper.observeMessages(this, viewModel, ttsQueue) {
-                TTSPlaybackHandler.speakNext(ttsManager, sttManager, viewModel, ttsQueue)
-                    //TTS 메시지 큐에 쌓인 메시지를 순서대로 재생하고, 상태에 따라 후속 동작을 연결하는 함수
-            }
-        }
-
-        //버튼동작과 상태로 정해둔 기능을 동작
-//        NavigationButtonBinder.bind(this, viewModel, sttManager, R.id.stt_button)
-
-
-        //지금은 버튼과 상태로 동작하게 만들었는데 이걸 나나중에 버튼이아니라 제스쳐로 바꾸기 쉽게
-        NavigationInputBinder.bindClickAndLongPress(this, viewModel, sttManager, R.id.stt_button)
 
 
     }
