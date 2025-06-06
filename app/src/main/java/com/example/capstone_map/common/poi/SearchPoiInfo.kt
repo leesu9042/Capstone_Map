7adefa42d4e37e57f4313bc4db64cba0489d8391
leesu9042 <tnckddl8957@gmail.com>
2025-06-06 15:44:45 +0900

    feat: gson에 사용할 데이터 클래스 추가

---
+++ app/src/main/java/com/example/capstone_map/common/poi/SearchPoiInfo.kt
@@ -0,0 +1,36 @@
+package com.example.capstone_map.common.poi
+
+
+
+data class TmapSearchPoiResponse(
+    val searchPoiInfo: SearchPoiInfo
+)
+
+data class SearchPoiInfo(
+    val totalCount: String,
+    val count: String,
+    val page: String,
+    val pois: Pois
+)
+
+data class Pois(
+    val poi: List<Poi>
+)
+
+data class Poi(
+    val id: String,
+    val newAddressList: NewAddressList?, // ← 있을 수도 없을 수도 있음
+    val pnsLat: String,
+    val pnsLon: String, //출입구 좌표
+    val name: String,
+    val telNo: String
+)
+
+data class NewAddressList(
+    val newAddress: List<NewAddress>
+)
+
+data class NewAddress(
+    val fullAddressRoad: String
+)
+
